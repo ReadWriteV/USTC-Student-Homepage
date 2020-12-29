ce0aa263ebf61d0e5149ccac07cc7efffbbbf5eb
ReadWriteV <ReadWriteV@outlook.com>
2020-12-29 15:44:16 +0800

    fix: header sticky

--- js/common.js
+++ js/common.js
@@ -3,6 +3,8 @@ function injectHeaderAndFooter(a) {
         $(document).ready(function () {
             $("#header").load("/html/header.html", function () {
                 $("li").eq(a).attr("class", "active");
+                $("#header").attr("class", "sticky-top shadow-sm");
+
             });
             $("#footer").load("/html/footer.html");
         });
